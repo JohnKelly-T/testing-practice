89807e3c5cf42d425082001587008e5b8dbc4cfd
john-kelly <johnkellytemena@gmail.com>
2025-06-04 17:22:40 +0800

    Add helper function for calculating average

--- js/analyze-array.js
+++ js/analyze-array.js
@@ -1,7 +1,13 @@
 function analyzeArray(arr) {
-  let object = {};
+  let object = {
+    average: findAverage(arr)
+  };
 
   return object;
+}
+
+function findAverage(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
 }
 
 
