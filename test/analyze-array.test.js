4792f618941d26b36bc917c5a3dce5019f295d77
john-kelly <johnkellytemena@gmail.com>
2025-06-04 17:31:51 +0800

    Write test to check all properties are defined

--- test/analyze-array.test.js
+++ test/analyze-array.test.js
@@ -24,4 +24,17 @@ describe('Analyze Array function', () => {
       analyzeArray([1,8,3,4,2,6]).length
     ).toBe(6);
   });
+
+  test('returns object with all properties defined', () => {
+    expect(
+      analyzeArray([1,8,3,4,2,6])
+    ).toStrictEqual(
+      {
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+      }
+    )
+  })
 });
